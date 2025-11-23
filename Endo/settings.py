62d001447be2b8353a4bf759542ca050a632ba23
gzuweve <hzewwen@gmail.com>
2025-11-23 14:12:26 +0300

    добавление STATIC_ROOT

--- Endo/settings.py
+++ Endo/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR / 'static']      # для разработки (если есть папка /static рядом с manage.py)
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = '/var/www/u0381436/data/endo/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
